cbfc35bd001284dfd66e660571409ebba5913eb9
Jesús Díez <jesus@jesusdiez.com>
2020-11-19 16:14:43 +0100

    refactor: Simplify my enums by extending from lib/Enum

--- src/Coin.php
+++ src/Coin.php
@@ -3,61 +3,22 @@ declare(strict_types=1);
 
 namespace Vending;
 
-use InvalidArgumentException;
+use Lib\Enum;
 
-final class Coin
+final class Coin extends Enum
 {
     public const CENT5 = 5;
     public const CENT10 = 10;
     public const CENT25 = 25;
     public const UNIT = 100;
-    private int $value;
-
-    public function __construct(int $value)
-    {
-        $this->guardIsValid($value);
-        $this->value = $value;
-    }
 
     public static function fromString(string $value): self
     {
         return new self((int) ((float) $value * 100));
     }
 
-    public static function options(): array
-    {
-        return [
-            'CENT5' => static::CENT5,
-            'CENT10' => static::CENT10,
-            'CENT25' => static::CENT25,
-            'UNIT' => static::UNIT,
-        ];
-    }
-
-    public static function values(): array
-    {
-        return \array_values(self::options());
-    }
-
-    public static function isValid($value): bool
-    {
-        return \in_array($value, self::values());
-    }
-
     public function __toString(): string
     {
         return number_format($this->value() / 100, 2, '.', ',');
-    }
-
-    public function value(): int
-    {
-        return $this->value;
-    }
-
-    private function guardIsValid(int $value)
-    {
-        if (!self::isValid($value)) {
-            throw new InvalidArgumentException(sprintf('%s is not a valid value', $value));
-        }
     }
 }
--- src/Item.php
+++ src/Item.php
@@ -3,59 +3,16 @@ declare(strict_types=1);
 
 namespace Vending;
 
-use InvalidArgumentException;
+use Lib\Enum;
 
-final class Item
+/**
+ * @method static Item WATER()
+ * @method static Item JUICE()
+ * @method static Item SODA()
+ */
+final class Item extends Enum
 {
     public const WATER = 'WATER';
-    public const SODA = 'SODA';
     public const JUICE = 'JUICE';
-    private string $value;
-
-    public function __construct(string $value)
-    {
-        $this->guardIsValid($value);
-        $this->value = $value;
-    }
-
-    public static function fromString(string $value): self
-    {
-        return new self($value);
-    }
-
-    public static function options(): array
-    {
-        return [
-            'WATER' => static::WATER,
-            'SODA' => static::SODA,
-            'JUICE' => static::JUICE,
-        ];
-    }
-
-    public static function values(): array
-    {
-        return \array_values(self::options());
-    }
-
-    public static function isValid($value): bool
-    {
-        return \in_array($value, self::values());
-    }
-
-    public function __toString(): string
-    {
-        return $this->value();
-    }
-
-    public function value(): string
-    {
-        return $this->value;
-    }
-
-    private function guardIsValid(string $value)
-    {
-        if (!self::isValid($value)) {
-            throw new InvalidArgumentException(sprintf('%s is not a valid value', $value));
-        }
-    }
+    public const SODA = 'SODA';
 }
