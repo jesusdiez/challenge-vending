9dc0800c44e4c95c07c2abef3dd371d5d21486ab
Jesús Díez <jesus@jesusdiez.com>
2020-11-22 19:37:10 +0100

    fix: Money does not need equal() as Enum has equals()

--- src/Domain/Money.php
+++ src/Domain/Money.php
@@ -37,11 +37,6 @@ final class Money
         return $this->cents;
     }
 
-    public function equal(Money $money): bool
-    {
-        return $this->cents === $money->cents();
-    }
-
     public function greaterThan(self $money): bool
     {
         return $this->cents > $money->cents();
